accebd563d05128f22137f0bd3afeb0875038b05
saadeddinne <saadeddine.feki@gmail.com>
2020-11-04 07:41:50 -0500

    before query

--- app/controllers/geolocation_controller.rb
+++ app/controllers/geolocation_controller.rb
@@ -1,38 +1,39 @@
 class GeolocationController < ApplicationController
+  before_action :authenticate_user!
 
-  def sqlQuery()
-    sql = "select b.administrator_full_name as nomadmin, 
-                b.technical_contact_full_name as nomtech, 
-                CONCAT(ad.number_and_street,' ', ad.city ,' ',ad.postal_code,' ', ad.country )  as addresse,
-                # ad.latitude latitude,
-                # ad.longitude longitude,
-                bitem.nbbat,
-                bitem.nbcol,
-                bitem.nbelev
-        from buildings b
-        join addresses ad on ad.entity_id = b.customer_id
-        join (
-            select batitemcount.building_id, 
-                count(batitemcount.bat_id) nbbat, 
-                sum(batitemcount.nbcol) nbcol,
-                sum(batitemcount.nbelev) nbelev
-            from (
-                SELECT bat.id bat_id, bat.building_id , 
-                    count(col.id) nbcol,
-                    (
-                        select count(e.id) nbelev from `columns` c 
-                        join elevators e on e.column_id  = c.id
-                        where c.battery_id = bat.id
-                        GROUP by c.battery_id
-                    ) nbelev
-                from batteries bat 
-                join `columns` col on col.battery_id = bat.id 
-                GROUP by bat.id, bat.building_id 
-            ) as batitemcount
-            GROUP by batitemcount.building_id
-        ) as bitem on bitem.building_id = b.id"
-    return sql
-  end 
+  # def sqlQuery()
+  #   sql = "select b.administrator_full_name as admin, 
+  #               b.technical_contact_full_name as techname, 
+  #               CONCAT(ad.number_and_street,' ', ad.city ,' ',ad.postal_code,' ', ad.country )  as address,
+  #                ad.latitude latitude,
+  #               ad.longitude longitude,
+  #               bitem.nbbat,
+  #               bitem.nbcol,
+  #               bitem.nbelev
+  #       from buildings b
+  #       join addresses ad on ad.id = b.customer_id
+  #       join (
+  #           select batitemcount.building_id, 
+  #               count(batitemcount.bat_id) nbbat, 
+  #               sum(batitemcount.nbcol) nbcol,
+  #               sum(batitemcount.nbelev) nbelev
+  #           from (
+  #               SELECT bat.id bat_id, bat.building_id , 
+  #                   count(col.id) nbcol,
+  #                   (
+  #                       select count(e.id) nbelev from `columns` c 
+  #                       join elevators e on e.column_id  = c.id
+  #                       where c.battery_id = bat.id
+  #                       GROUP by c.battery_id
+  #                   ) nbelev
+  #               from batteries bat 
+  #               join `columns` col on col.battery_id = bat.id 
+  #               GROUP by bat.id, bat.building_id 
+  #           ) as batitemcount
+  #           GROUP by batitemcount.building_id
+  #       ) as bitem on bitem.building_id = b.id"
+  #   return sql
+  # end 
 
 
 
