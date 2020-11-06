bb06db3628b5c756cbc74116ab5b4d8f02265c0d
saadeddinne <saadeddine.feki@gmail.com>
2020-11-06 14:47:25 -0500

    Google map and sendgrid is ok

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -92,21 +92,4 @@ class LeadsController < ApplicationController
 
 end
   
-    # def dropbox 
-    #     client = DropboxApi::Client.new
-    #     result = client.list_folder "/50"
-
-    #     #puts client.inspect
-    #     #puts "------------------------"
-    #     #result = clien.create_folder "/50"
-
-        
-    #     lead = Lead.where().firt
-
-    #     # public/uploads/lead/attachment
-
-    #     client.uploads(lead.attached_fille)
-
-    #     puts resultat.inspect
-    # end
-  
+    
