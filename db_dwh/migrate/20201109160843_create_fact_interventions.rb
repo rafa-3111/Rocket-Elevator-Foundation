cf157b75d59796d7bccc86e0615a2ed24ae4b28f
“frimina” <friminazaddi@gmail.com>
2020-11-09 11:50:39 -0500

    correction table interventions

--- db_dwh/migrate/20201109160843_create_fact_interventions.rb
+++ db_dwh/migrate/20201109160843_create_fact_interventions.rb
@@ -1,18 +1,20 @@
 class CreateFactInterventions < ActiveRecord::Migration[5.2]
   def change
     create_table :fact_interventions do |t|
-      t.int :EmployeeID
-      t.int :BuildingID
-      t.int :BatteryID
-      t.int :ColumnID
-      t.int :ElevatorID
-      t.string :Statut
-      t.string :Results
-      t.string :Rapport
+      t.int :employee_id
+      t.int :building_id
+      t.int :battery_id
+      t.int :column_id
+      t.int :elevator_id
+      t.string :status
+      t.string :results
+      t.string :repport
+      t.string :intervention_start
+      t.string :intervention_finish
+      t.string :status default: "Pending"
 
       t.timestamps
     end
   end
 end
-
 
